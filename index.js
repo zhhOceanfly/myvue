a2138318682f715abbae72e592f9c2edc330d7a0
oceanfly <805651381@qq.com>
2021-08-25 14:40:22 +0800

    #cm

---
+++ index.js
@@ -0,0 +1,278 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      id2: "haha",
+      message: '页面加载于 ' + new Date().toLocaleString()
+    }
+  })
+
+  var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true,
+      isActive: true,
+      isDisable: true,
+      styleObject: {
+        color: "red",
+        fontSize: 20
+      }
+    }
+  })
+
+  var app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        { text: '学习 JavaScript' },
+        { text: '学习 Vue' },
+        { text: '整个牛项目' }
+      ]
+    }
+  })
+
+  var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Hello Vue.js!'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+  })
+
+  var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
+
+  Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+  })
+  
+  var app7 = new Vue({
+    el: '#app-7',
+    data: {
+      groceryList: [
+        { id: 0, text: '蔬菜' },
+        { id: 1, text: '奶酪' },
+        { id: 2, text: '随便其它什么人吃的东西' }
+      ]
+    }
+  })
+
+  var obj = {
+    foo: 'bar'
+  }
+  
+  Object.freeze(obj)
+  
+  var app8 = new Vue({
+    el: '#app-8',
+    data: obj
+  })
+
+  var app9 = new Vue({
+    el: '#app-9',
+    data: {
+        isButtonDisabled: null 
+    }
+  })
+
+  
+  var app10 = new Vue({
+    el: '#app-10',
+    data: {
+      eventname: 'click',
+      message: 'Hello Vue.js!',
+    },
+    computed: {
+      reversedMessage: function () {
+        // `this` 指向 vm 实例
+        return this.message.split('').reverse().join('')
+      }
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+  })
+
+  var vm = new Vue({
+    el: '#demo',
+    data: {
+      firstName: 'Foo',
+      lastName: 'Bar',
+      fullName: 'Foo Bar'
+    },
+    watch: {
+      firstName: function (val) {
+        this.fullName = val + ' ' + this.lastName
+      },
+      lastName: function (val) {
+        this.fullName = this.firstName + ' ' + val
+      }
+    }
+  })
+
+  var vm2 = new Vue({
+    el: '#demo2',
+    data: {
+      firstName: 'Foo',
+      lastName: 'Bar'
+    },
+    computed: {
+      fullName: {// getter
+        get: function () {
+          return this.firstName + ' ' + this.lastName
+        },
+        // setter
+        set: function (newValue) {
+          var names = newValue.split(' ')
+          this.firstName = names[0]
+          this.lastName = names[names.length - 1]
+        }
+      }
+    }
+  })
+
+  var watchExampleVM = new Vue({
+    el: '#watch-example',
+    data: {
+      question: '',
+      answer: 'I cannot give you an answer until you ask a question!'
+    },
+    watch: {
+      // 如果 `question` 发生改变，这个函数就会运行
+      question: function (newQuestion, oldQuestion) {
+        this.answer = 'Waiting for you to stop typing...'
+        this.debouncedGetAnswer()
+      }
+    },
+    created: function () {
+      // `_.debounce` 是一个通过 Lodash 限制操作频率的函数。
+      // 在这个例子中，我们希望限制访问 yesno.wtf/api 的频率
+      // AJAX 请求直到用户输入完毕才会发出。想要了解更多关于
+      // `_.debounce` 函数 (及其近亲 `_.throttle`) 的知识，
+      // 请参考：https://lodash.com/docs#debounce
+      this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+    },
+    methods: {
+      getAnswer: function () {
+        if (this.question.indexOf('?') === -1) {
+          this.answer = 'Questions usually contain a question mark. ;-)'
+          return
+        }
+        this.answer = 'Thinking...'
+        var vm = this
+        axios.get('https://yesno.wtf/api')
+          .then(function (response) {
+            vm.answer = _.capitalize(response.data.answer)
+          })
+          .catch(function (error) {
+            vm.answer = 'Error! Could not reach the API. ' + error
+          })
+      }
+    }
+  })
+
+  var vue12 = new Vue({
+    el: '#v-for-object',
+    data: {
+      object: {
+        title: 'How to do lists in Vue',
+        author: 'Jane Doe',
+        publishedAt: '2016-04-10'
+      }
+    }
+  })
+
+  var vue13 = new Vue({
+    el: '#v-if',
+    data: {
+
+    }
+  })
+
+var vue15 = new Vue({
+  el: '#app-15',
+  data: {
+    numbers: [ 1, 2, 3, 4, 5 ]
+  },
+  computed: {
+    evenNumbers: function () {
+      return this.numbers.filter(function (number) {
+        return number % 2 === 0
+      })
+    }
+  }
+})
+
+var vue16 = new Vue({
+  el: '#app-16',
+  data: {
+    sets: [[ 1, 2, 3, 4, 5 ], [6, 7, 8, 9, 10]]
+  },
+  methods: {
+    even: function (numbers) {
+      return numbers.filter(function (number) {
+        return number % 2 === 0
+      })
+    }
+  }
+})
+
+var vue17 = new Vue({
+  el: '#app-17',
+  data: {
+    items: [
+      { msg: '学习 JavaScript' },
+      { msg: '学习 Vue' },
+      { msg: '整个牛项目' }
+    ]
+  },
+  methods: {
+    even: function (numbers) {
+      return numbers.filter(function (number) {
+        return number % 2 === 0
+      })
+    }
+  }
+})
+
+var vue18 = new Vue({
+  el: '#app-18',
+  data: {
+    counter: 0
+  },
+  methods: {
+    warn: function (message, event) {
+      // 现在我们可以访问原生事件对象
+      if (event) {
+        console.log(event)
+        event.preventDefault()
+      }
+      alert(message)
+    }
+  }
+})
+
+var vue19 = new Vue({
+  el: '#app-19',
+  data: {
+    checkedNames: [],
+    selected: ''
+  }
+})
